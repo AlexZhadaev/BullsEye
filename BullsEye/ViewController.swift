93a90d10eaec88c8cafaee8785451424312f8178
AlexZhadaev <pvt.vatnik@gmail.com>
2020-09-23 23:11:07 +0300

    add buttons

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func showAlert() {
+        let alert = UIAlertController(title: "Hello, World!", message: "This is my first app!", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func knockKnockButton(_ sender: Any) {
+        let alert = UIAlertController(title: "Knock Knock!", message: "Who's there?", preferredStyle: .actionSheet)
+        let action = UIAlertAction(title: "Dead Moroz!", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
 }
 
