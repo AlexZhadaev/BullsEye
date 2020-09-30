38344e027048fb833dae2e563e7542ffe503d767
AlexZhadaev <pvt.vatnik@gmail.com>
2020-09-30 22:29:18 +0300

    Coding Practice chapter is finished

---
+++ BullsEye/AboutAuthorViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AboutAuthorViewController.swift
+//  BullsEye
+//
+//  Created by Жадаев Алексей on 30.09.2020.
+//  Copyright © 2020 Жадаев Алексей. All rights reserved.
+//
+
+import UIKit
+
+class AboutAuthorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+}
